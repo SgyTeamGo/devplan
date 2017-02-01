3b19221e91681bea35b1d764549b38404b5618ad
Aram Hovhannisyan <aram.hovhannisyan@arm.synisys.com>
2017-02-01 15:13:33 +0400

    converted application to spring boot

---
+++ devplan-ui/src/main/java/app/Application.java
@@ -0,0 +1,14 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by aram.hovhannisyan on 2/1/2017.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
